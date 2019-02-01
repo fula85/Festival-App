4586ccca2e9fad46673a75d6af7b5dfba4d0a1aa
mobapp06 <mobapp06@dt-c205-14.local>
2019-02-01 12:12:07 +0100

    Made a list of Arrays

--- Festival App/ViewController.swift
+++ Festival App/ViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    let data: [[String]] = [["Reel big fish", "Swashbuckle", "Admiral freebie"], ["Bar A", "Bar B","Bar C", "Bar D"], ["Restaurant A", "Restaurant B", "Restaurant C", "Restaurant D"]]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[section].count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = data[indexPath.section][indexPath.row]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
