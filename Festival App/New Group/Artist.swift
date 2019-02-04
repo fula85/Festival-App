8a5ee74fb2897b20f27acdbcbb6f3076090a4d46
mobapp06 <mobapp06@dt-c205-14.local>
2019-02-04 15:59:53 +0100

    created a artist class

---
+++ Festival App/New Group/Artist.swift
@@ -0,0 +1,16 @@
+//
+//  Artist.swift
+//  Festival App
+//
+//  Created by mobapp06 on 04/02/2019.
+//  Copyright © 2019 EhB. All rights reserved.
+//
+
+import Foundation
+
+//class Artist {
+    
+    //var naam: String
+    //var omschrijving: String
+    
+//}
